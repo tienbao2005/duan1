e89d738c94a0307f9f9a53c540c1c8c29ea8a0a1
tien <tiennmph50685@gmail.com>
2024-11-18 10:38:40 +0700

    cap nhap client

--- models/SanPham.php
+++ models/SanPham.php
@@ -7,14 +7,16 @@ class SanPham
    {
 $this->conn = connectDB();
    } 
-public function getAllProduct(){
+   public function getAllSanPham(){
     try{
-        $sql= "SELECT * FROM san_phams ";
-        $stmt = $this -> conn ->prepare($sql);
+        $sql = "SELECT san_phams.*, danh_mucs.ten_danh_muc FROM san_phams
+        INNER JOIN danh_mucs ON san_phams.danh_muc_id = danh_mucs.id";
+        $stmt = $this->conn->prepare($sql); 
         $stmt->execute();
-        return $stmt-> fetchAll();
-    }catch (Exxception $e){
-        echo "lỗi" . $e->getMessage();
+        
+        return $stmt->fetchAll();
+    }catch(Exception $e){
+        echo "Lỗi: ".$e->getMessage();
     }
 }
 
