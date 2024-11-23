20245a96900e1fb020b230201e8bcd0455caac1f
tien <tiennmph50685@gmail.com>
2024-11-23 22:11:08 +0700

    hiển thị sản phẩm theo danh mục

---
+++ views/sanPhamTheoDanhMuc.php
@@ -0,0 +1,266 @@
+<?php require_once 'layout/header.php'  ?>
+<?php require_once 'layout/menu.php'  ?>
+
+
+
+
+<main>
+    <!-- hero slider area start -->
+    <section class="slider-area">
+        <div class="hero-slider-active slick-arrow-style slick-arrow-style_hero slick-dot-style">
+            <!-- single slider item start -->
+            <div class="hero-single-slide hero-overlay">
+                <div class="hero-slider-item bg-img" data-bg="img2/banner/1.png">
+                    <div class="container">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- single slider item start -->
+            <!-- single slider item start -->
+            <div class="hero-single-slide hero-overlay">
+            <div class="hero-slider-item bg-img" data-bg="img2/banner/2.png">
+                    <div class="container">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- single slider item start -->
+
+            <!-- single slider item start -->
+            <div class="hero-single-slide hero-overlay">
+            <div class="hero-slider-item bg-img" data-bg="img2/banner/3.png">
+                    <div class="container">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- single slider item start -->
+        </div>
+    </section>
+    <!-- hero slider area end -->
+
+
+
+    <!-- service policy area start -->
+    <div class="service-policy section-padding">
+        <div class="container">
+            <div class="row mtn-30">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="policy-item">
+                        <div class="policy-icon">
+                            <i class="pe-7s-plane"></i>
+                        </div>
+                        <div class="policy-content">
+                            <h6>Giao hàng</h6>
+                            <p>Giao hàng nhanh chóng</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="policy-item">
+                        <div class="policy-icon">
+                            <i class="pe-7s-help2"></i>
+                        </div>
+                        <div class="policy-content">
+                            <h6>Hỗ trợ</h6>
+                            <p>Hỗ trợ 24/7</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="policy-item">
+                        <div class="policy-icon">
+                            <i class="pe-7s-back"></i>
+                        </div>
+                        <div class="policy-content">
+                            <h6>Hoàn tiền</h6>
+                            <p>Hoàn tiền trong 30 ngày</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="policy-item">
+                        <div class="policy-icon">
+                            <i class="pe-7s-credit"></i>
+                        </div>
+                        <div class="policy-content">
+                            <h6>Thanh toán</h6>
+                            <p>Bảo mật thanh toán</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- service policy area end -->
+
+
+    <!-- banner statistics area end -->
+
+    <!-- product area start -->
+    <section class="product-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- section title start -->
+                    <div class="section-title text-center">
+                        <h2 class="title">Sản phẩm của chúng tôi</h2>
+                        <p class="sub-title">Sản phẩm được cập nhật hàng tuần</p>
+                    </div>
+                    <!-- section title start -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="product-container">
+
+
+                        <!-- product tab content start -->
+                        <div class="tab-content">
+                            <div class="tab-pane fade show active" id="tab1">
+                                <div class="product-carousel-4 slick-row-10 slick-arrow-style">
+                                    <!-- product item start -->
+                                    <?php foreach ($spdm as $key => $sanPham) { ?>
+                                        <div class="product-item">
+                                            <figure class="product-thumb">
+                                                <a href="<?=BASE_URL.'?act=chi-tiet-san-pham&id_san_pham='  .$sanPham['id']?>">
+                                                    <img class="pri-img" src="<?= BASE_URL . $sanPham['hinh_anh'] ?>" alt="product">
+                                                    <img class="sec-img" src="<?= BASE_URL . $sanPham['hinh_anh'] ?>" alt="product">
+                                                </a>
+                                                <div class="product-badge">
+                                                    <?php
+                                                    $ngayNhap = new DateTime($sanPham['ngay_nhap']);
+                                                    $ngayHienTai = new DateTime();
+                                                    $tinhNgay = $ngayHienTai->diff($ngayNhap);
+                                                    if ($tinhNgay->days <= 7) {    ?>
+
+                                                        <div class="product-label new">
+                                                            <span>Mới</span>
+                                                        </div>
+                                                    <?php } ?>
+                                                    <?php if($sanPham['gia_khuyen_mai'] >0) { ?>
+                                                    <div class="product-label discount">
+                                                        <span>Giảm giá</span>
+                                                    </div>
+                                                    <?php }?>
+                                                </div>
+                                            
+                                                <div class="cart-hover">
+                                                    
+                                                    <a href="<?=BASE_URL.'?act=chi-tiet-san-pham&id_san_pham=' .$sanPham['id']?>">
+                                                    <button class="btn btn-cart">Xem chi tiết</button>
+                                                    </a>
+                                                </div>
+                                            </figure>
+                                            <div class="product-caption text-center">
+                                                
+                                                <h6 class="product-name">
+                                                    <a href="<?=BASE_URL.'?act=chi-tiet-san-pham&id_san_pham=' .$sanPham['id']?>"><?= $sanPham['ten_san_pham']?></a>
+                                                </h6>
+                                                <div class="price-box">
+                                                    <?php if($sanPham['gia_khuyen_mai'] >0){ ?>
+                                                            <span class="price-regular"><?=  formatPrice($sanPham['gia_khuyen_mai']).'đ';?></span>
+                                                            <span class="price-old"><del><?= formatPrice($sanPham['gia_san_pham']). 'đ';?></del></span>
+                                                    <?php }else{ ?>
+                                                        <span class="price-regular"><?= formatPrice($sanPham['gia_san_pham']).'đ'?></span>
+                                                        
+                                                        
+                                                    <?php }    ?>
+                                                
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                    <!-- product item end -->
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- product tab content end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- product area end -->
+
+
+
+
+    <!-- brand logo area start -->
+    <div class="brand-logo section-padding pt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="brand-logo-carousel slick-row-10 slick-arrow-style">
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/1.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/2.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/3.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/4.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/5.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+
+                        <!-- single brand start -->
+                        <div class="brand-item">
+                            <a href="#">
+                                <img src="assets/img/brand/6.png" alt="">
+                            </a>
+                        </div>
+                        <!-- single brand end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- brand logo area end -->
+</main>
+
+
+
+<!-- offcanvas mini cart start -->
+<?php require_once 'layout/miniCart.php' ?>
+<!-- offcanvas mini cart end -->
+
+<?php require_once 'layout/footer.php' ?>
